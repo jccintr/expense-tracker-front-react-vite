d164e3fd4c71f6490a7184c908c3f01c4312393d
Julio Cesar <jccintr@gmail.com>
2025-03-18 23:45:51 -0300

    aplicativo concluido com sucesso

--- src/components/EmptyTable.tsx
+++ src/components/EmptyTable.tsx
@@ -7,7 +7,7 @@ const EmptyTable = ({message,message2,buttonLabel,onAdd}) => {
     <div className='flex flex-col w-full items-center justify-center my-auto'>
         <img src={empty} className='h-[300px] mt-10'/>
         <Label className='my-4 text-xl text-gray-500'>{message}</Label>
-        <Label className='my-4 text-lg text-gray-500'>{message2}</Label>
+        <Label className='my-4 text-base text-gray-500'>{message2}</Label>
         <Button variant='outline' onClick={onAdd}>{buttonLabel}</Button>
     </div>
   )
